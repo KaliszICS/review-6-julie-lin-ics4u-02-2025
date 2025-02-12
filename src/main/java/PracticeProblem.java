4ae6a77c8ce46d716d12effa69ff9b701e9ac8b6
Julie Lin <julie.lin@student.tdsb.on.ca>
2025-02-12 02:42:51 +0000

    Thank you for the most easiest assignment today :D

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -20,27 +20,27 @@ public class PracticeProblem {
 	}
 	// Q4
 	public static String removeFirst(String sentence) {
-		return sentence.subtring(1);
+		return sentence.substring(1);
 	}
 	// Q5
-	public static String combinedLength(String word1, String word2) {
+	public static int combinedLength(String word1, String word2) {
 		return word1.length() + word2.length();
 	}
 	// Q6
-	public static int isEven(int num1) {
+	public static boolean isEven(int num1) {
 		return num1%2 == 0;
 	}
 	// Q7
-	public static int isOdd(int num1) {
+	public static boolean isOdd(int num1) {
 		return num1%2 != 0;
 	}
 	// Q8
-	public static int isPositive(int num1) {
-		return num1 < 0;
+	public static boolean isPositive(int num1) {
+		return num1 > 0;
 	}
 	// Q9
-	public static int isNegative(int num1) {
-		return num1 > 0;
+	public static boolean isNegative(int num1) {
+		return num1 < 0;
 	}
 
 }
